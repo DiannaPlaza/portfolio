5f8e5ad6c687b5b84c21f5e42f765f943d40b222
Dianna Plaza <dianna.s.plaza@gmail.com>
2022-06-12 20:40:24 -0400

    added burger menu styling and js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// hamburger menu
+
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('navUl');
+
+hamburger.addEventListener('click', () => {
+    navUl.classList.toggle('show')
+})
