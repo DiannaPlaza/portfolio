bcf6cd962fb4f52d3aa79f4409c1651ba494bb14
Dianna Plaza <dianna.s.plaza@gmail.com>
2022-10-17 21:15:11 -0400

    test

--- scripts.js
+++ scripts.js
@@ -1,11 +1,5 @@
 // hamburger menu
 
-const hamburger = document.getElementById('hamburger');
-const navUl = document.getElementById('navUl');
-
-hamburger.addEventListener('click', () => {
-    navUl.classList.toggle('show')
-})
 
 
 
